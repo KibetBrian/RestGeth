9ddb55369f9078dd48e54e75b388ff4f8920d4e8
KibetBrian <briankibet2040@gmail.com>
2022-06-11 20:02:26 +0300

    adds kystore generation and decryption

---
+++ core/keystore.go
@@ -0,0 +1,51 @@
+package core
+
+import (
+	"io/ioutil"
+	"log"
+	"path/filepath"
+
+	"github.com/ethereum/go-ethereum/accounts/keystore"
+	"github.com/ethereum/go-ethereum/crypto"
+)
+const (
+	path = "keydir"
+)
+
+
+
+//Creates new keystore and stores it in the keystores directory
+func GenerateKeyStore(passphrase string) {
+	keystore := keystore.NewKeyStore(path, keystore.StandardScryptN, keystore.StandardScryptP)
+	account, err := keystore.NewAccount(passphrase)
+	if err != nil{
+		log.Printf("Failed to generate new key from keystore. Err: %v\n", err)
+	}
+	log.Println("Generated keystore with addresss:  ",account.Address.Hex())	
+}
+
+//This function descypts the file to get the addresses
+func DecryptKeystore(passphrase string){
+
+	fileInfo, err := ioutil.ReadDir(path)
+	if err != nil{
+		log.Printf("Failed to read the directory. Err: %v",err)
+	}
+	fileName := fileInfo[0].Name()
+	filePath := filepath.Join(path,fileName)
+	
+	fileBytes, err := ioutil.ReadFile(filePath)
+	if err != nil{
+		log.Printf("Failed to read file. Err: %v",err)
+	}
+
+	key, err := keystore.DecryptKey(fileBytes, passphrase)
+	if err != nil{
+		log.Printf("Failed to read file. Err: %v",err)
+	}
+	privateKey := key.PrivateKey
+	publicKey := privateKey.PublicKey
+	publicAddress := crypto.PubkeyToAddress(publicKey)
+	log.Println("The public address of decripted key is: ", publicAddress.Hex())
+}
+
--- main.go
+++ main.go
@@ -1,23 +1,14 @@
 package main
 
 import (
-	"encoding/json"
-	"log"
 
 	"github.com/KibetBrian/geth/core"
-	"github.com/KibetBrian/geth/utils"
 )
 const (
 	address = "0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B"
 )
 
 func main(){
-	utils.ConfigureEnv()
-	
-	wallet := core.CreateWallet()
-	values, err := json.MarshalIndent(wallet, "\t", "\n")
-	if err != nil{
-		panic(err)
-	}
-	log.Println(string(values))
+		core.GenerateKeyStore("passphrase")
+		core.DecryptKeystore("passphrase")
 }
